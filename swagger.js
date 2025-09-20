91c11f53a894773fd8eb5ab9fef1e89873216c0e
Seryn Armstrong <serynarmstrong@gmail.com>
2025-09-20 11:42:19 +0200

    updated swagger

--- swagger.js
+++ swagger.js
@@ -1,12 +1,12 @@
 const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
-    info: {
-        title: 'Anime Api',
-        description: 'Anime Api'
-    },
-    host: 'localhost:3000',
-    schemes: ['https', 'http']
+  info: {
+    title: 'Anime & Profiles API',
+    description: 'Anime & Profiles API'
+  },
+  host: 'localhost:3000',
+  schemes: ['http', 'https']
 };
 
 const outputFile = './swagger.json';
